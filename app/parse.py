5fd91660777935aee7b5b337100b6f216b413d76
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-27 22:56:09 +0200

    Used Quotes & fixed problems

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 import csv
-import time
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
@@ -21,6 +20,7 @@ def get_soup(url):
     response.raise_for_status()
     return BeautifulSoup(response.text, "html.parser")
 
+
 def parse_quotes_from_page(soup):
     quotes_data = []
     quote_elements = soup.find_all("div", class_="quote")
@@ -28,26 +28,20 @@ def parse_quotes_from_page(soup):
         text = quote.find("span", class_="text").get_text()
         author = quote.find("small", class_="author").get_text()
         tags = [tag.get_text() for tag in quote.find_all("a", class_="tag")]
-        author_page_relative = quote.find("a")["href"]
-        author_page_url = urljoin(BASE_URL, author_page_relative)
-        quotes_data.append({
-            "text": text,
-            "author": author,
-            "tags": str(tags),
-            "author_url": author_page_url
-        })
+        quotes_data.append(Quote(text, author, tags))
     return quotes_data
+
 
 def get_author_bio(author_url):
     soup = get_soup(author_url)
     bio_div = soup.find("div", class_="author-description")
     return bio_div.get_text(strip=True) if bio_div else ""
 
+
 def scrape_all_quotes():
     quotes = []
     authors_cache = {}
     page_url = BASE_URL
-
     while page_url:
         print(f"Scraping page: {page_url}")
         soup = get_soup(page_url)
@@ -55,21 +49,22 @@ def scrape_all_quotes():
 
         for quote in page_quotes:
             quotes.append(quote)
-            author = quote["author"]
-
+            author = quote.author
             if author not in authors_cache:
-                bio = get_author_bio(quote["author_url"])
+                author_page_relative = soup.find("a", href=True, text=author)["href"]
+                author_page_url = urljoin(BASE_URL, author_page_relative)
+                bio = get_author_bio(author_page_url)
                 authors_cache[author] = bio
 
         next_button = soup.find("li", class_="next")
-
         if next_button:
             next_relative = next_button.find("a")["href"]
             page_url = urljoin(BASE_URL, next_relative)
-
         else:
             page_url = None
+
     return quotes, authors_cache
+
 
 def write_quotes_to_csv(quotes, output_csv_path):
     fieldnames = ["text", "author", "tags"]
@@ -78,10 +73,11 @@ def write_quotes_to_csv(quotes, output_csv_path):
         writer.writeheader()
         for quote in quotes:
             writer.writerow({
-                "text": quote["text"],
-                "author": quote["author"],
-                "tags": quote["tags"]
+                "text": quote.text,
+                "author": quote.author,
+                "tags": ",".join(quote.tags)
             })
+
 
 def write_authors_to_csv(authors, output_csv_path):
     authors_csv_path = output_csv_path.rsplit(".", 1)[0] + "_authors.csv"
